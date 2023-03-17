c2856cda13fcf0fb2b4fa0613b4474d1dd6a3881
Arley <aguerrero0615@gmail.com>
2023-03-17 02:16:34 -0500

    Feat: Se ajustan estilos en todos los dispositivos

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,46 +1,46 @@
 // Rapi API
-// const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UC9IdEZPl44BPor7mvxhnOYg&part=snippet%2Cid&order=date&maxResults=8'
-// const content = null || document.getElementById('content')
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UC9IdEZPl44BPor7mvxhnOYg&part=snippet%2Cid&order=date&maxResults=8'
+const content = null || document.getElementById('content')
 
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': 'ae0f79ef54msh7e149c8ee486f13p1e8786jsnccaa65bf2ae5',
-// 		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
-// 	}
-// };
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'ae0f79ef54msh7e149c8ee486f13p1e8786jsnccaa65bf2ae5',
+		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+	}
+};
 
-// async function fetchData(urlApi){
-//     const response = await fetch(urlApi, options)
-//     const data = await response.json()
-//     return data
-// }
+async function fetchData(urlApi){
+    const response = await fetch(urlApi, options)
+    const data = await response.json()
+    return data
+}
 
-// (async () => {
-//     try{
-//         const videos = await fetchData(API)
-//         let view = `
-//             ${videos.items.map(video => `
-//                 <div class="group relative">
-//                     <a href="https://youtube.com/watch?v=${video.id.videoId}" target="_blank">
-//                         <div class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-//                             <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
-//                         </div>
-//                         <div class="mt-4 flex justify-between">
-//                             <h3 class="text-sm text-gray-700">
-//                                 <span aria-hidden="true" class="absolute inset-0"></span>
-//                             ${video.snippet.title}
-//                             </h3>
-//                         </div>
-//                     </a>
-//                 </div>
-//             `).slice(0,4).join('')}
-//         `;
-//         content.innerHTML = view
-//     } catch(error){
-//         console.log(error)
-//     }
-// })()
+(async () => {
+    try{
+        const videos = await fetchData(API)
+        let view = `
+            ${videos.items.map(video => `
+                <div class="containerVideos group relative">
+                    <a href="https://youtube.com/watch?v=${video.id.videoId}" target="_blank">
+                        <div class="sectionVideos w-full aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+                            <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+                        </div>
+                        <div class="mt-4 flex justify-between">
+                            <h3 class="textVideo">
+                                <span aria-hidden="true" class="absolute inset-0"></span>
+                            ${video.snippet.title}
+                            </h3>
+                        </div>
+                    </a>
+                </div>
+            `).slice(0,4).join('')}
+        `;
+        content.innerHTML = view
+    } catch(error){
+        console.log(error)
+    }
+})()
 
 // END Rapi API
 
